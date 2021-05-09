bbc8ace08be9bdd0f735e83cfae3d34b4b2e8ca2
UnArtisant <73105273+UnArtisant@users.noreply.github.com>
2021-05-09 17:56:40 +0200

    clockwise

--- headers/rubiks.h
+++ headers/rubiks.h
@@ -1,18 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include "../src/conio.h"
 
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef RUBIKS_PROJECT
+#define RUBIKS_PROJECT
 
 typedef struct Face Face;
 
-int * create_tab(int size);
 char * index_color(int index);
+
 int side_to_index(char side);
-Face *** create_rubiks(int row, int column);
 int select_color(char color);
+int random_num(int min, int max);
+int opposide_side(int side);
+int * create_tab(int size);
+
+Face *** create_rubiks(int row, int column);
+
+
 void init_rubiks(Face *** rubiks);
 void display_rubiks(Face *** rubiks, int row, int column);
+void fill_rubiks(Face *** rubiks);
+void scramble_rubiks(Face *** rubiks);
+
+//mouvement
+void front_anticlockwise(Face *** rubiks, int rotation );
+void back_anticlockwise(Face *** rubiks, int rotation );
+void up_anticlockwise(Face *** rubiks, int rotation );
+void down_anticlockwise(Face *** rubiks, int rotation );
+void right_anticlockwise(Face *** rubiks, int rotation );
+void left_anticlockwise(Face *** rubiks, int rotation );
+
+void front__clockwise(Face *** rubiks, int rotation );
+void back__clockwise(Face *** rubiks, int rotation );
+void up__clockwise(Face *** rubiks, int rotation );
+void down__clockwise(Face *** rubiks, int rotation);
+void right__clockwise(Face *** rubiks, int rotation );
+void left__clockwise(Face *** rubiks, int rotation);
+void vertical_rotation(Face *** rubiks);
+void horizontal_rotation(Face *** rubiks);
+void rotate_horizontal(Face *** rubiks, int side);
+void rotate(Face *** rubiks, int side);
 
 #endif
--- src/rubiks.c
+++ src/rubiks.c
@@ -1,6 +1,6 @@
 #include "../headers/rubiks.h"
 
-typedef enum { B  = 4, W = 0, R = 3, G = 2, Y = 5, O = 1 } T_COLOR ;
+typedef enum { B  = 4, W = 0, R = 3, G = 2, Y = 5, O = 1, LG=6 } T_COLOR ;
 typedef enum { UP  = 4, FRONT = 0, RIGHT = 3, DOWN = 2, BACK = 5, LEFT = 1 } C_SIDE;
 
 struct Face {
@@ -23,8 +23,10 @@ char * index_color(int index) {
             return "B";
         case 5 :
             return "Y";
+        case 6 :
+            return "LG";    
     }
-    return "W"; 
+    return "LG"; 
 }
 
 /**
@@ -35,6 +37,10 @@ int * create_tab(int size) {
   return (int *)malloc(size);
 }
 
+int random_num(int min, int max) {
+  srand(time(NULL));
+  return (rand() % (max + 1 - min)) + min; 
+}
 /**
  * Raphael Barriet, Ahyl Pradhan.
  * Return index from side.
@@ -120,17 +126,24 @@ void init_rubiks(Face *** rubiks) {
   } 
 }
 
+/**
+ * 
+ * Raphael Barriet, Ahyl Pradhan. 
+ * display rubiks;
+ * 
+*/
 void display_rubiks(Face *** rubiks, int row, int column){
 
     int i,j;
     Face * square;
     char * over = "\n";
-    char * empty_square = "\n   ";
+    char * empty_square = "\n      ";
     for(i = 0; i < row; i++) {
       printf("%s", empty_square);
       for(j = 0; j < column; j++) {
           square = &rubiks[0][i][j];   
           printf("%s", index_color(square->t_color));
+          printf("%s", " ");
       } 
       printf("%s", over); 
     }
@@ -138,19 +151,23 @@ void display_rubiks(Face *** rubiks, int row, int column){
     for(i = 0; i < row; i++) {
        for(j = 0; j < column; j++) {
            Face * square = &rubiks[1][i][j];
-           printf("%s", index_color(square->t_color)); 
+           printf("%s", index_color(square->t_color));
+           printf("%s", " "); 
        }
        for(j = 0; j < column; j++) {
            Face * square = &rubiks[2][i][j];
            printf("%s", index_color(square->t_color));
+           printf("%s", " ");
        }
        for(j = 0; j < column; j++) {
            Face * square = &rubiks[3][i][j];
            printf("%s", index_color(square->t_color));
+           printf("%s", " ");
        }
        for(j = 0; j < column; j++) {
            Face * square = &rubiks[4][i][j];
            printf("%s", index_color(square->t_color));
+           printf("%s", " ");
        }
        printf("%s", over); 
     } 
@@ -160,6 +177,230 @@ void display_rubiks(Face *** rubiks, int row, int column){
       for(j = 0; j < column; j++) {
           square = &rubiks[5][i][j];   
           printf("%s", index_color(square->t_color));
+          printf("%s", " ");
       } 
     }   
+}
+
+void blank_rubiks(Face *** rubiks) {
+    int i,j, k;
+    for(i = 0; i < 6; i++) {
+       for(j = 0; j < 3; j++){
+         for(k = 0; k < 3; k++) {
+            rubiks[i][j][k].t_color = LG;
+        }
+      }
+    } 
+}
+
+void fill_rubiks(Face *** rubiks) {
+    return;
+} 
+
+
+
+void front__clockwise(Face *** rubiks, int rotation){
+    int i;
+    for(i = 0; i < rotation; i++) {
+      rotate_horizontal(rubiks, FRONT);
+    }
+}
+
+void back__clockwise(Face *** rubiks, int rotation ){
+    int i;
+    for(i = 0; i < rotation; i++) {
+      rotate_horizontal(rubiks, BACK);
+    }
+}
+
+void up__clockwise(Face *** rubiks, int rotation ){
+    int i;
+    Face temp, temp_2, temp_3;
+    for(i=0; i < rotation; i++) {
+        rotate(rubiks, UP);
+    }
+
+    for(i=0; i < 3; i++) {
+     temp = rubiks[LEFT][i][0];
+     rubiks[LEFT][i][0] = rubiks[FRONT][0][i];
+
+     temp_2 = rubiks[BACK][2][i];
+     rubiks[BACK][2][i] = temp; 
+
+     temp_3 = rubiks[RIGHT][i][2];
+     rubiks[RIGHT][i][2] = temp_2;
+
+     rubiks[FRONT][0][i] = temp_3; 
+    }
+}
+
+void down__clockwise(Face *** rubiks, int rotation ){
+    int i;
+    Face temp, temp_2, temp_3;
+    for(i=0; i < rotation; i++) {
+        rotate(rubiks, DOWN);
+    }
+
+    for(i=0; i < 3; i++) {
+     temp = rubiks[LEFT][i][2];
+     rubiks[LEFT][i][2] = rubiks[FRONT][2][i];
+
+     temp_2 = rubiks[BACK][0][i];
+     rubiks[BACK][0][i] = temp; 
+
+     temp_3 = rubiks[RIGHT][i][0];
+     rubiks[RIGHT][i][0] = temp_2;
+
+     rubiks[FRONT][2][i] = temp_3; 
+    }
+}
+
+void right__clockwise(Face *** rubiks, int rotation ){
+    int i;
+    Face temp, temp_2, temp_3;
+    for(i=0; i < rotation; i++) {
+        rotate(rubiks, RIGHT);
+    }
+
+    for(i=0; i < 3; i++) {
+     temp = rubiks[DOWN][i][2];
+     rubiks[DOWN][i][2] = rubiks[FRONT][i][2];
+
+     temp_2 = rubiks[BACK][i][2];
+     rubiks[BACK][i][2] = temp; 
+
+     temp_3 = rubiks[UP][i][0];
+     rubiks[UP][i][0] = temp_2;
+
+     rubiks[FRONT][i][2] = temp_3; 
+    }
+}
+
+void left__clockwise(Face *** rubiks, int rotation ){
+    int i;
+    Face temp, temp_2, temp_3;
+    for(i=0; i < rotation; i++) {
+        rotate(rubiks, LEFT);
+    }
+
+    for(i=0; i < 3; i++) {
+     temp = rubiks[DOWN][i][0];
+     rubiks[DOWN][i][0] = rubiks[FRONT][2][i];
+
+     temp_2 = rubiks[BACK][0][i];
+     rubiks[BACK][0][i] = temp; 
+
+     temp_3 = rubiks[UP][i][2];
+     rubiks[UP][i][2] = temp_2;
+
+     rubiks[FRONT][2][i] = temp_3; 
+    }
+}
+
+void front_anticlockwise(Face *** rubiks, int rotation ){
+    int i;
+    for(i = 0; i < 3; i++) {
+        front__clockwise(rubiks, rotation);
+    }
+}
+
+void back_anticlockwise(Face *** rubiks, int rotation ){
+    int i;
+    for(i = 0; i < 3; i++) {
+        back__clockwise(rubiks, rotation);
+    }
+}
+
+void up_anticlockwise(Face *** rubiks, int rotation ){
+    int i;
+    for(i = 0; i < 3; i++) {
+        up__clockwise(rubiks, rotation);
+    }
+}
+
+void down_anticlockwise(Face *** rubiks, int rotation ){
+    int i;
+    for(i = 0; i < 3; i++) {
+        down__clockwise(rubiks, rotation);
+    }
+}
+
+void right_anticlockwise(Face *** rubiks, int rotation ){
+    int i;
+    for(i = 0; i < 3; i++) {
+        right__clockwise(rubiks, rotation);
+    }
+}
+
+void left_anticlockwise(Face *** rubiks, int rotation ){
+    int i;
+    for(i = 0; i < 3; i++) {
+        left__clockwise(rubiks, rotation);
+    }
+}
+
+void rotate_horizontal(Face *** rubiks, int side) {
+    Face * stock; 
+    Face * stock_2;
+    int k;
+
+    rotate(rubiks, side);
+
+    switch (side)
+    {
+    case 0:
+        k = 0;
+        break;
+    case 5:
+        k = 2;
+        break;
+    default:
+        k = 0;
+    } 
+
+    stock = rubiks[G][k];
+    rubiks[G][k] = rubiks[R][k];
+
+    stock_2 = rubiks[B][k];
+    rubiks[B][k] = rubiks[O][k];
+
+    rubiks[O][k] = stock;
+
+    rubiks[R][k] = stock_2;
+}
+
+void rotate(Face *** rubiks, int side) {
+    Face ** temp = (Face **)malloc(3 * sizeof(Face*));
+    Face ** temp_2;
+
+    int j, r, k;
+    for(r = 0; r < 3; r++) {
+        temp[r] = (Face *)malloc(3 * sizeof(Face));
+    }
+    for(j = 0; j < 3; j++) {
+        temp[2][2 - j] = rubiks[side][j][2];
+        temp[1][2 - j] = rubiks[side][j][1]; 
+        temp[0][2 - j] = rubiks[side][j][0];
+    }
+    temp_2 = rubiks[side];
+    rubiks[side] = temp; 
+    free(temp_2); 
+}
+
+void horizontal_rotation(Face *** rubiks){
+   Face ** temp = rubiks[0];
+   rubiks[0] = rubiks[5];
+   rubiks[5] = temp;
+   free(temp);
+}
+
+void vertical_rotation(Face *** rubiks) {
+   Face ** temp = rubiks[DOWN];
+   Face ** temp_2 = rubiks[FRONT];
+
+   rubiks[DOWN] = rubiks[UP];
+   rubiks[FRONT] = rubiks[BACK];
+
+   rubiks[UP] = temp;
+   rubiks[BACK] = temp_2;
 }
